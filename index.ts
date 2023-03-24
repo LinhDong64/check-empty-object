4cb71c14163daea4ad0c3bb3a1a236424f78f0f7
LinhDong64 <mailinhdong@gmail.com>
2023-03-24 13:57:19 +0700

    fix input module

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-export const isEmptyObject = (object: { [key: string]: string }) => {
+export const isEmptyObject = (object) => {
+  if (typeof object !== "object") return true;
   if (Object.keys(object).length === 0) return true;
   return false;
 };
